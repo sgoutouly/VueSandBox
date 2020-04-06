88c95e8e4c5ac8d941a870b126ab95589e352c6f
Sylvain <sylvain@mac-sylvain.home>
2020-04-06 10:31:29 +0200

    added some more projects

---
+++ InstantPrototyping/App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default { 
+		data : function() {
+			return {
+				name:'kiki'
+			}
+		},
+		methods: {
+			test:function() {
+		
+			}
+
+		}
+	}
+</script>>
+
+
+<template>
+<h1>Salut les {{name}} !</h1>	
+</template>
